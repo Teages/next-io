7fd1ecc58e6a4d8d9dd903af3c1434cf3b413ade
Teages <Teages@teages.xyz>
2022-11-09 14:11:58 +0800

    session layout

---
+++ layouts/session.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <div class="max-w-sm m-auto p-4 sm:mr-0">
+      <div class="w-full">
+        <div class="py-2">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+
+<script setup>
+import Layout from './background.vue'
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,77 +1,72 @@
 <template>
-  <div class="max-w-sm m-auto p-4 sm:mr-0">
-    <div class="w-full">
-      <div v-if="cookie" class="py-2">
-        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div class="card-body">
-            <h2 class="card-title">Profile</h2>
-            <div v-if="auth.user.value">
-              <div class="form-control" v-if="auth.user.value.uid">
-                <label class="label">
-                  <span class="label-text">Cytoid ID</span>
-                </label>
-                <input v-model="auth.user.value.uid" disabled type="text" class="input input-bordered" />
-              </div>
-            </div>
-            <div v-else>
-              Loading...
-            </div>
-            <div class="form-control mt-2">
-              <button class="btn btn-primary" @click="auth.loginWithCookie">Reload</button>
-            </div>
-            <div class="form-control mt-2">
-              <button class="btn btn-error" @click="auth.logout">Logout</button>
-            </div>
+  <div v-if="cookie">
+    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="card-body">
+        <h2 class="card-title">Profile</h2>
+        <div v-if="auth.user.value">
+          <div class="form-control" v-if="auth.user.value.uid">
+            <label class="label">
+              <span class="label-text">Cytoid ID</span>
+            </label>
+            <input v-model="auth.user.value.uid" disabled type="text" class="input input-bordered" />
+          </div>
+        </div>
+        <div v-else>
+          Loading...
+        </div>
+        <div class="form-control mt-2">
+          <button class="btn btn-primary" @click="auth.loginWithCookie">Reload</button>
+        </div>
+        <div class="form-control mt-2">
+          <button class="btn btn-error" @click="auth.logout">Logout</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <div class="my-2">
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <h2 class="card-title">Login</h2>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Cytoid ID</span>
+            </label>
+            <input v-model="loginForm.username" type="text" placeholder="Cytoid ID" class="input input-bordered" />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input v-model="loginForm.password" type="password" placeholder="Password" class="input input-bordered" />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">Need help?</a>
+            </label>
+          </div>
+          <div class="form-control">
+            <label class="label cursor-pointer">
+              <span class="label-text">Remember me</span>
+              <input type="checkbox" :checked="loginForm.remember" class="checkbox checkbox-primary" />
+            </label>
+          </div>
+          <div class="form-control mt-2">
+            <button class="btn btn-primary" @click="login">Login</button>
           </div>
         </div>
       </div>
-      <div v-else>
-        <div class="my-2">
-          <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div class="card-body">
-              <h2 class="card-title">Login</h2>
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">Cytoid ID</span>
-                </label>
-                <input v-model="loginForm.username" type="text" placeholder="Cytoid ID" class="input input-bordered" />
-              </div>
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">Password</span>
-                </label>
-                <input v-model="loginForm.password" type="password" placeholder="Password"
-                  class="input input-bordered" />
-                <label class="label">
-                  <a href="#" class="label-text-alt link link-hover">Need help?</a>
-                </label>
-              </div>
-              <div class="form-control">
-                <label class="label cursor-pointer">
-                  <span class="label-text">Remember me</span>
-                  <input type="checkbox" :checked="loginForm.remember" class="checkbox checkbox-primary" />
-                </label>
-              </div>
-              <div class="form-control mt-2">
-                <button class="btn btn-primary" @click="login">Login</button>
-              </div>
-            </div>
+    </div>
+    <div class="my-2">
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <h2 class="card-title">Login with Cookie (Debug)</h2>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Cookie</span>
+            </label>
+            <input v-model="loginCookie" type="password" placeholder="Cookie" class="input input-bordered" />
           </div>
-        </div>
-        <div class="my-2">
-          <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div class="card-body">
-              <h2 class="card-title">Login with Cookie (Debug)</h2>
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">Cookie</span>
-                </label>
-                <input v-model="loginCookie" type="password" placeholder="Cookie" class="input input-bordered" />
-              </div>
-              <div class="form-control mt-2">
-                <button class="btn btn-primary" @click="loginWithCookie">Login</button>
-              </div>
-            </div>
+          <div class="form-control mt-2">
+            <button class="btn btn-primary" @click="loginWithCookie">Login</button>
           </div>
         </div>
       </div>
@@ -108,6 +103,6 @@ const loginWithCookie = () => {
 }
 
 definePageMeta({
-  layout: "background"
+  layout: "session"
 })
 </script>
